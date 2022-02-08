39ee3c457b9aebecadbdbb96b605bd9c9d1a103b
vadym <vadim.melnichenko@gmail.com>
2022-02-08 20:03:25 +0000

    added static root

--- blog/settings.py
+++ blog/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'staticfiles/'))
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
@@ -128,7 +129,6 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'mediafiles/'))
 MEDIA_URL = 'media/'
 
-print(MEDIA_ROOT)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
