15f85153c441ae56de627ab37d3b4aca0c39da75
vadym <vadim.melnichenko@gmail.com>
2022-02-09 17:26:53 +0000

    added django debug var for envorement

--- post/admin.py
+++ post/admin.py
@@ -6,7 +6,7 @@ from .models import Author, Categories, Post
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    readonly_fields = ('user', )
+    # readonly_fields = ('user', )
     list_display = ['id', 'nickname', ]
     list_display_links = ['nickname', ]
 
@@ -19,6 +19,6 @@ class CategoriesAdmin(admin.ModelAdmin):
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    readonly_fields = ('author', )
+    # readonly_fields = ('author', )
     list_display = ['id', 'header', 'is_active']
     list_display_links = ['header', ]
